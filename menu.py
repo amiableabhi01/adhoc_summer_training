45fcaf3edca50940fbebd970efd1fdc26aaa4162
amiableabhi01 <40180841+amiableabhi01@users.noreply.github.com>
2018-06-22 02:18:36 +0530

    menu for different functions using python3.

---
+++ menu.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import time 
+import webbrowser
+import commands
+menu='''
+press 1 to check current time and date :
+press2 to scan all ur mac adresses in ur current connected network :
+press 3 to shutdown ur machine after 15 min :
+press 4 to search something on google :
+press 5 to lgout ur current machine :
+press 6 to shutdown all connected sysytem in ur current network :
+press 7 to update ststus in fb :
+press 8 to list ip address of given website :
+'''
+print(menu)
+choice=input()
+
+if choice == '1' :
+    print("current date and time is :",time.ctime())
+elif choice == '4':
+	find=input("enter ur query: ")
+	webbrowser.open_new_tab("https://www.google.com/search?q="+find)
+else :
+	print("invalid option")
+if choice == '2':
+   mac_add= commands.getoutput("cat /sys/class/net/*/address")
+   print(mac_add)
